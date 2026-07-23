e22f07252dd58c32d13530c5b7f516199ba30951
Jhaz <jhazmineaudrey7@gmail.com>
2026-07-23 23:55:12 +0800

    feat: add initial player movement

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,1 +1,11 @@
 extends CharacterBody2D
+
+@export var speed : float = 200
+
+func get_player_input():
+	var direction := Input.get_vector("Left", "Right", "Up", "Down")
+	velocity = direction * speed
+
+func _physics_process(delta: float) -> void:
+	get_player_input()
+	move_and_slide()
