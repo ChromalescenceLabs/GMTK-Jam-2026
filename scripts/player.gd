bc5a35a6318d21ab5da103092693610bfdb51b79
Jhaz <jhazmineaudrey7@gmail.com>
2026-07-24 00:52:13 +0800

    feat: add testing scene & player movement
    The testing scene is essentially for just testing stuff that'd be added in the main game. Also added the basic player movement code.

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,11 +1,12 @@
 extends CharacterBody2D
 
-@export var speed : float = 200
+const SPEED : float = 100
+
+func _physics_process(_delta: float) -> void:
+	get_player_input()
 
 func get_player_input():
 	var direction := Input.get_vector("Left", "Right", "Up", "Down")
-	velocity = direction * speed
-
-func _physics_process(delta: float) -> void:
-	get_player_input()
+	velocity = direction * SPEED
+			
 	move_and_slide()
