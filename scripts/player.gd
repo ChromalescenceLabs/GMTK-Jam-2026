8330decc8f8716986c3e5ca75fa03c13d901cfdf
Jhaz <jhazmineaudrey7@gmail.com>
2026-07-24 16:06:07 +0800

    feat: update player movement
    Made player movement more smooth

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,12 +1,19 @@
 extends CharacterBody2D
 
-const SPEED : float = 100
+const SPEED : int = 150
+const ACCELERATION : int = 5
+const FRICT : int = 8
 
-func _physics_process(_delta: float) -> void:
-	get_player_input()
+func _physics_process(delta: float) -> void:
+	get_player_input(delta)
 
-func get_player_input():
-	var direction := Input.get_vector("Left", "Right", "Up", "Down")
-	velocity = direction * SPEED
+func get_player_input(delta):
+	var input = Vector2(
+		Input.get_action_strength("Right") - Input.get_action_strength("Left"),
+		Input.get_action_strength("Down") - Input.get_action_raw_strength("Up")
+	).normalized()
+	
+	var lerp_weight = delta * (ACCELERATION if input else FRICT)
+	velocity = lerp(velocity, input * SPEED, lerp_weight)
 			
 	move_and_slide()
